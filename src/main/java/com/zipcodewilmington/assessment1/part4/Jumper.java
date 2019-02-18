8d87a249f7bff9358c3211e41c4bfe818acf8441
Nirmala <nirmala.talari@gmail.com>
2019-02-18 15:33:07 -0500

    Palindrome

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -1,11 +1,33 @@
 package com.zipcodewilmington.assessment1.part4;
 
+import javax.sound.midi.Soundbank;
+
 public class Jumper {
 
     /*
      * Complete the function below.
      */
-    public int jumps(int k, int j) {
-        return -1;
+    public int jumps(int k, int j)
+    {
+        int count = 0;
+        Integer var = Integer.MAX_VALUE;
+        if (k>j) {
+            while (var != 0) {
+                var = k - j;
+                k = var;
+
+                count++;
+            }
+        }
+
+        else
+        {
+            var = j - k;
+            count ++;
+        }
+
+        System.out.printf("Bob must jump %d times to reach %d height flag",count,k);
+
+        return count;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -3,6 +3,30 @@ package com.zipcodewilmington.assessment1.part5;
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+        String str = "";
+        int length = input.length();
+        int count = input.length();
+
+
+        for (int i =0;i<length;i++)
+        {
+            for(int j = i+2;j<=length;j++)
+            {
+                str = input.substring(i,j);
+                count += IsPalindrome(str);
+            }
+        }
+
+        return count;
+    }
+
+    public Integer IsPalindrome(String str)
+    {
+        for (int i =0;i<str.length()/2;i++)
+        {
+            if (str.charAt(i)!=str.charAt(str.length()-1-i))
+                return 0;
+        }
+        return 1;
     }
 }
